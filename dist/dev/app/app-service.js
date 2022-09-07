c3495c62dc06b0cb66835964389afef3b936240f
xafzz <15553626266@163.com>
2022-09-08 01:48:04 +0800

    json

---
+++ dist/dev/app/app-service.js
@@ -0,0 +1,146 @@
+if (typeof Promise !== "undefined" && !Promise.prototype.finally) {
+  Promise.prototype.finally = function(callback) {
+    const promise = this.constructor;
+    return this.then(
+      (value) => promise.resolve(callback()).then(() => value),
+      (reason) => promise.resolve(callback()).then(() => {
+        throw reason;
+      })
+    );
+  };
+}
+;
+if (typeof uni !== "undefined" && uni && uni.requireGlobal) {
+  const global = uni.requireGlobal();
+  ArrayBuffer = global.ArrayBuffer;
+  Int8Array = global.Int8Array;
+  Uint8Array = global.Uint8Array;
+  Uint8ClampedArray = global.Uint8ClampedArray;
+  Int16Array = global.Int16Array;
+  Uint16Array = global.Uint16Array;
+  Int32Array = global.Int32Array;
+  Uint32Array = global.Uint32Array;
+  Float32Array = global.Float32Array;
+  Float64Array = global.Float64Array;
+  BigInt64Array = global.BigInt64Array;
+  BigUint64Array = global.BigUint64Array;
+}
+;
+if (uni.restoreGlobal) {
+  uni.restoreGlobal(Vue, weex, plus, setTimeout, clearTimeout, setInterval, clearInterval);
+}
+(function(vue, shared) {
+  "use strict";
+  var _export_sfc = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+      target[key] = val;
+    }
+    return target;
+  };
+  const _sfc_main$1 = /* @__PURE__ */ vue.defineComponent({
+    __name: "index",
+    setup(__props) {
+      const title = vue.ref("Hello");
+      return (_ctx, _cache) => {
+        return vue.openBlock(), vue.createElementBlock("view", { class: "content" }, [
+          vue.createElementVNode("image", {
+            class: "logo",
+            src: "/static/logo.png"
+          }),
+          vue.createElementVNode("view", { class: "text-area" }, [
+            vue.createElementVNode("text", { class: "title" }, vue.toDisplayString(title.value), 1)
+          ])
+        ]);
+      };
+    }
+  });
+  var PagesIndexIndex = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["__file", "/Users/xaf/wwwroot/android-uni-vite-vue3/src/pages/index/index.vue"]]);
+  __definePage("pages/index/index", PagesIndexIndex);
+  const ON_SHOW = "onShow";
+  const ON_HIDE = "onHide";
+  const ON_LAUNCH = "onLaunch";
+  function isDebugMode() {
+    return typeof __channelId__ === "string" && __channelId__;
+  }
+  function jsonStringifyReplacer(k, p) {
+    switch (shared.toRawType(p)) {
+      case "Function":
+        return "function() { [native code] }";
+      default:
+        return p;
+    }
+  }
+  function normalizeLog(type, filename, args) {
+    if (isDebugMode()) {
+      args.push(filename.replace("at ", "uni-app:///"));
+      return console[type].apply(console, args);
+    }
+    const msgs = args.map(function(v) {
+      const type2 = shared.toTypeString(v).toLowerCase();
+      if (["[object object]", "[object array]", "[object module]"].indexOf(type2) !== -1) {
+        try {
+          v = "---BEGIN:JSON---" + JSON.stringify(v, jsonStringifyReplacer) + "---END:JSON---";
+        } catch (e) {
+          v = type2;
+        }
+      } else {
+        if (v === null) {
+          v = "---NULL---";
+        } else if (v === void 0) {
+          v = "---UNDEFINED---";
+        } else {
+          const vType = shared.toRawType(v).toUpperCase();
+          if (vType === "NUMBER" || vType === "BOOLEAN") {
+            v = "---BEGIN:" + vType + "---" + v + "---END:" + vType + "---";
+          } else {
+            v = String(v);
+          }
+        }
+      }
+      return v;
+    });
+    return msgs.join("---COMMA---") + " " + filename;
+  }
+  function formatAppLog(type, filename, ...args) {
+    const res = normalizeLog(type, filename, args);
+    res && console[type](res);
+  }
+  const createHook = (lifecycle) => (hook, target = vue.getCurrentInstance()) => {
+    !vue.isInSSRComponentSetup && vue.injectHook(lifecycle, hook, target);
+  };
+  const onShow = /* @__PURE__ */ createHook(ON_SHOW);
+  const onHide = /* @__PURE__ */ createHook(ON_HIDE);
+  const onLaunch = /* @__PURE__ */ createHook(ON_LAUNCH);
+  const _sfc_main = /* @__PURE__ */ vue.defineComponent({
+    __name: "App",
+    setup(__props) {
+      onLaunch(() => {
+        formatAppLog("log", "at App.vue:4", "App Launch");
+      });
+      onShow(() => {
+        formatAppLog("log", "at App.vue:7", "App Show");
+      });
+      onHide(() => {
+        formatAppLog("log", "at App.vue:10", "App Hide");
+      });
+      return () => {
+      };
+    }
+  });
+  var App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__file", "/Users/xaf/wwwroot/android-uni-vite-vue3/src/App.vue"]]);
+  function createApp() {
+    const app = vue.createVueApp(App);
+    return {
+      app
+    };
+  }
+  const { app: __app__, Vuex: __Vuex__, Pinia: __Pinia__ } = createApp();
+  uni.Vuex = __Vuex__;
+  uni.Pinia = __Pinia__;
+  __app__.provide("__globalStyles", __uniConfig.styles);
+  __app__._component.mpType = "app";
+  __app__._component.render = () => {
+  };
+  __app__.mount("#app");
+})(Vue, uni.VueShared);
